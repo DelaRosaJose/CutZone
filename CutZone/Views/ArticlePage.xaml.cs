95379ab9aa72d2d607736664178a01bf03c9b3db
DelaRosa_Jose <josealberto3053@gmail.com>
2023-10-03 14:44:25 -0400

    Aplicamos Custom Cursor para los botones

--- CutZone/Views/ArticlePage.xaml.cs
+++ CutZone/Views/ArticlePage.xaml.cs
@@ -1,11 +1,10 @@
-using CutZone.Models;
 using CutZone.ViewModels;
 
 namespace CutZone.Views;
 
 public partial class ArticlePage : ContentPage
 {
-	public ArticlePage(ArticleViewModel vm)
+    public ArticlePage(ArticleViewModel vm)
     {
         BindingContext = vm;
         InitializeComponent();
