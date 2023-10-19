b75c37209e78b70e8c805c35a689867a772c2f80
DelaRosa_Jose <Usuario@USER>
2023-10-19 14:12:39 -0400

    Añadimos animaciones

--- CutZone/Views/ArticlePage.xaml.cs
+++ CutZone/Views/ArticlePage.xaml.cs
@@ -12,4 +12,29 @@ public partial class ArticlePage : ContentPage
 
     }
 
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+
+
+        IconState.IconKind = 
+        LabelState.IsVisible= true;
+        await LabelState.ScaleTo(1, 3000, Easing.CubicInOut);
+        LabelState.Scale = 0.5;
+        LabelState.IsVisible = false;
+
+
+
+
+        //State.TranslateTo()
+        //await State.TranslateTo(15,15,2000,Easing.Linear);
+
+        //await to
+        //await State.TranslateTo(-100, -100, 1000);
+        //State.AnchorX = 150;
+
+        //var animation = new Animation(x => { this.State.AnchorX = x; }, 0, 60);
+
+        //animation.Commit(this, "OpenFlyout", length: 250);
+
+    }
 }
